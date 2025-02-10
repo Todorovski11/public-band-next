58ea28241639f8137a36be1b6a2c69647e37bd1e
Todorovski11 <todorovski.martin11@gmail.com>
2025-02-10 12:04:02 +0100

    changing the gallery section

---
+++ src/app/ui/parallax-scroll.js
@@ -0,0 +1,56 @@
+"use client";
+import { useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
+
+export const ParallaxScroll = ({ images, className }) => {
+  const gridRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    container: gridRef,
+    offset: ["start start", "end start"],
+  });
+
+  const translateFirst = useTransform(scrollYProgress, [0, 1], [0, -300]);
+  const translateSecond = useTransform(scrollYProgress, [0, 1], [0, 300]);
+  const translateThird = useTransform(scrollYProgress, [0, 1], [0, -300]);
+
+  const third = Math.ceil(images.length / 3);
+  const firstPart = images.slice(0, third);
+  const secondPart = images.slice(third, 2 * third);
+  const thirdPart = images.slice(2 * third);
+
+  return (
+    <div className={cn("h-[50rem] items-start overflow-y-auto w-full", className)} ref={gridRef}>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start max-w-7xl mx-auto gap-12 py-60 px-12">
+        <div className="grid gap-12">
+          {firstPart.map((el, idx) => (
+            <motion.div style={{ y: translateFirst }} key={"grid-1" + idx}>
+              <Image src={el} className="h-96 w-full object-cover rounded-xl" height="500" width="500" alt="thumbnail" />
+            </motion.div>
+          ))}
+        </div>
+        <div className="grid gap-12">
+          {secondPart.map((el, idx) => (
+            <motion.div style={{ y: translateSecond }} key={"grid-2" + idx}>
+              <Image src={el} className="h-96 w-full object-cover rounded-xl" height="500" width="500" alt="thumbnail" />
+            </motion.div>
+          ))}
+        </div>
+        <div className="grid gap-12">
+          {thirdPart.map((el, idx) => (
+            <motion.div style={{ y: translateThird }} key={"grid-3" + idx}>
+              <Image src={el} className="h-96 w-full object-cover rounded-xl" height="500" width="500" alt="thumbnail" />
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
